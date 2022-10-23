510cb69084f754b7c58a6737547a7a29adc96349
lrljoe <104938042+lrljoe@users.noreply.github.com>
2022-10-23 23:02:20 +0100

    Update MysqlHandler.php

--- src/Logger/Monolog/Handler/MysqlHandler.php
+++ src/Logger/Monolog/Handler/MysqlHandler.php
@@ -20,7 +20,7 @@ class MysqlHandler extends AbstractProcessingHandler
         parent::__construct($level, $bubble);
     }
 
-    protected function write(array $record) : void
+    protected function write(array $record) :void
     {
         $data = [
             'instance'    => gethostname(),
